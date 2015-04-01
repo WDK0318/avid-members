fd56942e2c40ca534f11a6d45965b23d6b44f412
root <root@vagrant.vm>
2015-04-01 11:22:24 +0000

    3. Testing - Add unit tests for Model/Member

--- tests/unit/Model/MemberTest.php
+++ tests/unit/Model/MemberTest.php
@@ -2,6 +2,12 @@
 
 namespace Avid\CandidateChallenge\Model;
 
+use Avid\CandidateChallenge\Model\Address;
+use Avid\CandidateChallenge\Model\Email;
+use Avid\CandidateChallenge\Model\Height;
+use Avid\CandidateChallenge\Model\Member;
+use Avid\CandidateChallenge\Model\Weight;
+
 /**
  * @covers \Avid\CandidateChallenge\Model\Member
  *
@@ -14,8 +20,98 @@ namespace Avid\CandidateChallenge\Model;
  */
 final class MemberTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var array
+     */
+    private $fixtures;
+
     protected function setUp()
     {
-        $this->markTestIncomplete('Member tests must be implemented');
+        parent::setUp();
+
+        $this->fixtures = array(
+            'username' => 'will.hoppe',
+            'password' => 'well',
+            'address' => new Address('Canada', 'Ontario', 'Whitechurch-Stouffville', 'A2E 3L2'),
+            'date_of_birth' => new \DateTime(date('Y-m-d', strtotime('54 years ago'))),
+            'limits' => 'Something Long Term',
+            'height' => new Height('6\' 11"'),
+            'weight' => new Weight('125 lbs'),
+            'body_type' => 'Muscular',
+            'ethnicity' => 'Caucasian (white)',
+            'email' => new Email('ldietrich@hotmail.com'),
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_create_members_class_as_expected()
+    {
+        $member = new Member(
+            $this->fixtures['username'],
+            $this->fixtures['password'],
+            $this->fixtures['address'],
+            $this->fixtures['date_of_birth'],
+            $this->fixtures['limits'],
+            $this->fixtures['height'],
+            $this->fixtures['weight'],
+            $this->fixtures['body_type'],
+            $this->fixtures['ethnicity'],
+            $this->fixtures['email']
+        );
+
+        $this->assertInstanceOf(Member::CLASS_NAME, $member);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_properly_load_members_values()
+    {
+        $member = new Member(
+            $this->fixtures['username'],
+            $this->fixtures['password'],
+            $this->fixtures['address'],
+            $this->fixtures['date_of_birth'],
+            $this->fixtures['limits'],
+            $this->fixtures['height'],
+            $this->fixtures['weight'],
+            $this->fixtures['body_type'],
+            $this->fixtures['ethnicity'],
+            $this->fixtures['email']
+        );
+
+        $this->assertSame($this->fixtures['username'], $member->getUsername());
+        $this->assertSame($this->fixtures['password'], $member->getPassword());
+        $this->assertSame($this->fixtures['address'], $member->getAddress());
+        $this->assertSame($this->fixtures['date_of_birth'], $member->getDateOfBirth());
+        $this->assertSame($this->fixtures['limits'], $member->getLimits());
+        $this->assertSame($this->fixtures['height'], $member->getHeight());
+        $this->assertSame($this->fixtures['weight'], $member->getWeight());
+        $this->assertSame($this->fixtures['body_type'], $member->getBodyType());
+        $this->assertSame($this->fixtures['ethnicity'], $member->getEthnicity());
+        $this->assertSame($this->fixtures['email'], $member->getEmail());
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_count_member_age_correctly()
+    {
+        $member = new Member(
+            $this->fixtures['username'],
+            $this->fixtures['password'],
+            $this->fixtures['address'],
+            $this->fixtures['date_of_birth'],
+            $this->fixtures['limits'],
+            $this->fixtures['height'],
+            $this->fixtures['weight'],
+            $this->fixtures['body_type'],
+            $this->fixtures['ethnicity'],
+            $this->fixtures['email']
+        );
+
+        $this->assertEquals(54, $member->getAge());
     }
 }
